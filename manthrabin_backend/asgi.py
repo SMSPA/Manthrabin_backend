4330f6bad187fa728b122e55b6ec01ce79d3b753
Shima Maghzi <sh.maghzi2003@gmail.com>
2025-04-09 17:05:29 +0330

    configure asgi for websocket connection

--- manthrabin_backend/asgi.py
+++ manthrabin_backend/asgi.py
@@ -7,10 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
-import os
-
+from conversations import routing
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'manthrabin_backend.settings')
+from conversations.websocket.jwt_middleware import JWTAuthMiddleware
 
-application = get_asgi_application()
+# from your_app import consumers
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": JWTAuthMiddleware(
+        URLRouter([
+            routing.websocket_urlpatterns])
+    ),
+})
