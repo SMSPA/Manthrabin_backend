d978742377c47c02eb584d43b6991072f9304768
Shima Maghzi <sh.maghzi2003@gmail.com>
2025-04-09 16:52:55 +0330

    add config

--- manthrabin_backend/settings.py
+++ manthrabin_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -51,8 +52,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = config.EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = config.EMAIL_HOST_PASSWORD
 
 
 MIDDLEWARE = [
@@ -99,8 +100,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'manthrabin',
-        'USER': 'manthrabin',
-        'PASSWORD': 'QQB@ng8215',
+        'USER': config.DB_USER,
+        'PASSWORD': config.DB_PASSWORD,
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
