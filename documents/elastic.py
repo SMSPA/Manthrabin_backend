1dad5b468eb2b0c07358b3a22d0a3fff05801e61
Arshia Shafiei <arshiashafiei03@gmail.com>
2025-06-06 17:36:54 +0330

    feat: refactor Elasticsearch integration and document processing pipelines
    - Modifies the `add_docs_pipeline` to accept a `public_id` for document metadata.
    - Updates the `delete_docs_pipeline` to use `delete_by_query` based on the `public_id` for deletion.

--- documents/elastic.py
+++ documents/elastic.py
@@ -1,42 +1,36 @@
 from langchain_openai import OpenAIEmbeddings
 from langchain_elasticsearch import ElasticsearchStore
-# from haystack_integrations.document_stores.elasticsearch import ElasticsearchDocumentStore
+
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 from os import getenv
-from dotenv import load_dotenv
 from elasticsearch import Elasticsearch
 
-
-load_dotenv()
 
 es_client = Elasticsearch(hosts=[f"{getenv('ES_SCHEMA')}://{getenv('ES_URL')}:{getenv('ES_PORT')}"])
 
 embeddings = OpenAIEmbeddings(model='text-embedding-3-small')
 
-# Configure and initialize the Elasticsearch vector store
 vector_store = ElasticsearchStore(
     es_url=f"{getenv('ES_SCHEMA')}://{getenv('ES_URL')}:{getenv('ES_PORT')}",
-    index_name=getenv('ES_INDEX'),
+    index_name=getenv('ES_INDEX', 'manthrabin'),
     embedding=embeddings,
-    # es_user=getenv('ES_USER'),
-    # es_password=getenv('ES_PASS'),
-    es_params={"verify_certs": False}  # Disable certificate verification for Elasticsearch
+    es_user=getenv('ES_USER'),
+    es_password=getenv('ES_PASS'),
+    es_params={"verify_certs": False}
 )
 
 
-def add_docs_pipeline(file_path: str):
+def add_docs_pipeline(file_path: str, public_id: str):
     """Processes a PDF file, splits its content, and adds it to the vector store.
 
     Args:
         file_path (str): The path to the PDF file to be processed.
     """
-    # Load PDF document
     loader = PyPDFLoader(file_path)
     docs = loader.load()
 
-    # Split document into smaller chunks
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=2000,       # Maximum size of each text chunk
         chunk_overlap=500,     # Overlap between chunks for context preservation
@@ -44,20 +38,19 @@ def add_docs_pipeline(file_path: str):
         is_separator_regex=False,  # Whether the separator is a regex pattern
     )
 
-    # Generate text chunks
-    documents = text_splitter.split_documents(docs)
+    split_chunks = text_splitter.split_documents(docs)
 
-    # Store processed documents in Elasticsearch vector store
-    return vector_store.add_documents(documents=documents)
+    for chunk in split_chunks:
+        chunk.metadata["public_id"] = public_id
+
+    return vector_store.add_documents(documents=split_chunks)
 
 
-def delete_docs_pipeline(uuids: list):
-    """Deletes documents from the Elasticsearch vector store based on their UUIDs.
-
-    Args:
-        uuids (list): A list of document UUIDs to be deleted.
-
-    Returns:
-        dict: The response from the Elasticsearch delete operation.
-    """
-    return vector_store.delete(ids=uuids)
+def delete_docs_pipeline(public_id: str):
+    es = vector_store.client
+    body = {
+        "query": {
+            "term": { "metadata.public_id": public_id }
+        }
+    }
+    return es.delete_by_query(index=getenv("ES_INDEX", "manthrabin"), body=body)
