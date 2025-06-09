bfbba74e5be585b8c27ef7c838c5f30eca35d66f
Arshia Shafiei <arshiashafiei03@gmail.com>
2025-06-09 20:20:50 +0330

    fix: ASGI application initialization

--- manthrabin_backend/asgi.py
+++ manthrabin_backend/asgi.py
@@ -7,15 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 import os
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "manthrabin_backend.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "manthrabin_backend.settings")
 
 from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from conversations.websocket.jwt_middleware import JWTAuthMiddleware
 import conversations.routing
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": JWTAuthMiddleware(
         URLRouter(
             conversations.routing.websocket_urlpatterns)
